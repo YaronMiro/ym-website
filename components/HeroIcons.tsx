5ab2e09459a7512efdbf7e169a9c135f27486617
Yaron Miro <yaron@showdigs.com>
2025-02-23 22:38:54 +0700

    fix layout try 11

--- components/HeroIcons.tsx
+++ components/HeroIcons.tsx
@@ -13,26 +13,24 @@ interface IconData {
 }
 
 export const icons: IconData[] = [
-    { name: "head", arrowDirection: "" },
+    { name: "head", arrowDirection: "rotate-0" },
     { name: "app-code", arrowDirection: "-rotate-45" },
     { name: "cloud-shield", arrowDirection: "rotate-45" },
-    { name: "api", arrowDirection: "" },
+    { name: "api", arrowDirection: "rotate-0" },
     { name: "ui", arrowDirection: "-rotate-45" },
     { name: "cloud-balancer", arrowDirection: "rotate-45" },
-    { name: "bug-tracker", arrowDirection: "" },
-    { name: "todo-list", arrowDirection: null },
+    { name: "bug-tracker", arrowDirection: "rotate-0" },
+    { name: "todo-list", arrowDirection: "-rotate-45" },
 ];
 
 const HeroIcons: React.FC<HeroIconsProps> = ({ color }) => {
     return (
-        <div className={`${color} grid justify-center grid-cols-4 md:grid-cols-8 gap-y-5 xl:gap-y-0`}>
-            {icons.map(({ name, arrowDirection}, index) => {
-                const itemPosition = (index + 1);
-
+        <div className={`${color} grid grid-cols-4 md:grid-cols-8 gap-x-2 xl:gap-x-5 gap-y-5 xl:gap-y-0`}>
+            {icons.map(({ name, arrowDirection}) => {
                 return (
                     <div key={name} className="flex items-center">
-                        <YmIcon className="w-[3rem]" icon={name} />
-                        {arrowDirection !== null && <IoIosArrowRoundForward className={`text-3xl text-secondary/60 mx-3 ${arrowDirection} ${itemPosition === 4 && 'hidden xs:block'}`} />}
+                        <YmIcon className="w-[3rem] xl:w-[3.2rem]" icon={name} />
+                        <IoIosArrowRoundForward className={`ml-2 xl:ml-5 text-3xl text-secondary/60 ${arrowDirection}`} />
                     </div>
                 )
             }) }
