4a890687dffe7890f079b4609d70b22ab431c68b
Yaron Miro <yaron@showdigs.com>
2025-02-28 01:19:39 +0700

    new desgin 1

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="container pb-36 pt-20">
-        <main className="flex flex-col xl:flex-row items-center xl:items-stretch px-4">
+        <main className="flex flex-col xl:flex-row items-center xl:items-stretch xl:gap-8 px-4">
           {/* hero section */}
           <div className="flex flex-col justify-between">
             <div>
@@ -37,33 +37,20 @@ const Home: React.FC = () => {
                   Experienced in cloud technologies, DevOps, AI integration, and have a strong eye for design, all while consistently delivering high-quality, reliable software efficiently.
                 </p>
               </div>
-            </div>
-            <div className="mt-24">
-              {/* social links and resume download */}
-              <div className="flex xl:flex-col items-center xl:items-start mt-12 xl:mt-0">
-                <div className="flex justify-between w-full">
-                  <ResumeButtonDownload className="xl:hidden" />
-                  <Social className="flex gap-4 xl:gap-5" iconStyles="text-3xl" />
-                </div>
-              </div>
-              {/* contact info */}
-              <div className="flex justify-between xl:justify-start gap-3 mt-10 xl:mt-6 text-white/70" >
-                <Link href="mailto:yaron.miro@gmail.com" className="group hover:text-white/80">
-                  <span className="font-semibold text-secondary group-hover:text-secondary-hover">Email: </span>yaron.miro@gmail.com
-                </Link>
-                <span className="text-white/50">|</span>
-                <Link href="tel:+972--544297621" className="group hover:text-white/80">
-                  <span className="font-semibold text-secondary group-hover:text-secondary-hover">Phone: </span>+972-544297621
-                </Link>
+              <div className="flex gap-8 mt-16">
+                <footer className="flex justify-center bg-primary">
+                  {/* Icons */}
+                  <HeroIcons iconColor="stroke-white/20" arrowColor="text-accent/45" />
+                </footer>
               </div>
             </div>
           </div>
           {/* Image and particles */}
-          <div className="relative flex flex-1 items-end justify-end mt-12 xl:mt-0 mb-8 xl:mb-0">
+          <div className="relative flex flex-1 justify-end items-end">
             <ParticlesContainer className="absolute bottom-10 top-0 xl:top-[-100px] right-0 xl:right-[-50px] left-0 z-[-9999]" />
             <div className="relative">
               <Image
-                className="w-auto h-auto xl:h-[650px]"
+                className="w-auto h-auto xl:h-[550px]"
                 src={heroImage}
                 alt="Image of Yaron Miro"
               />
@@ -72,13 +59,24 @@ const Home: React.FC = () => {
             </div>
           </div>
         </main>
-        <div className="flex justify-between items-stretch gap-8 mt-6">
-          <div className="h-[2px] flex-1 bg-white/20 mt-16"></div>
-          <footer className="flex justify-center bg-primary mt-8">
-            {/* Icons */}
-            <HeroIcons iconColor="stroke-white/20" arrowColor="text-accent/45" />
-          </footer>
-          <div className="h-[2px] flex-1 bg-white/20 mt-16"></div>
+        <div className="flex flex-row leading-8 mt-24 gap-6">
+          {/* contact info */}
+          <div className="flex justify-between xl:justify-start gap-3 text-white/70" >
+            <Link href="mailto:yaron.miro@gmail.com" className="group hover:text-white/80">
+              <span className="font-semibold text-secondary group-hover:text-secondary-hover">Email: </span>yaron.miro@gmail.com
+            </Link>
+            <span className="text-white/50">|</span>
+            <Link href="tel:+972--544297621" className="group hover:text-white/80">
+              <span className="font-semibold text-secondary group-hover:text-secondary-hover">Phone: </span>+972-544297621
+            </Link>
+          </div>
+          {/* social links and resume download */}
+          <div className="flex xl:flex-col items-center xl:items-start">
+            <div className="flex justify-between w-full">
+              <ResumeButtonDownload className="xl:hidden" />
+              <Social className="flex gap-4 xl:gap-5" iconStyles="text-3xl" />
+            </div>
+          </div>
         </div>
       </div>
     </>
