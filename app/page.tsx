5968ddc640e33e9d22093378f02784cb8b9291c2
Yaron Miro <yaron@showdigs.com>
2025-03-07 12:44:32 +0700

    fix hero layout and design 61

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from 'next/image'
-// import { BsCodeSlash } from "react-icons/bs";
+import { BsCodeSlash } from "react-icons/bs";
 import { BsFillLightningFill } from "react-icons/bs";
 import ParticlesContainer from '@/components/ParticlesContainer'
 import heroImage from '@/public/hero-image.webp'
@@ -13,19 +13,19 @@ const Home: React.FC = () => {
   return (
     <>
       <div className="container pt-6 xl:pt-16">
-        <main className="relative flex flex-col xl:flex-row items-center xl:items-start">
+        <main className="relative flex flex-col xl:flex-row items-center">
           {/* hero section */}
           <div className="flex flex-col justify-center xl:w-full">
             <div className="max-w-[600px] md:max-w-[650px]">
               {/* main Title */}
               <h1 className="flex flex-col font-semibold">
                 <span className="text-5xl text-secondary">Hello I&apos;m</span>
-                <span className="text-[5rem] leading-[1.2] text-white/50">Yaron Miro</span>
+                <span className="text-[5rem] leading-[1.2] text-accent">Yaron Miro</span>
               </h1>
               {/* role */}
               <div className="flex items-center mt-10 gap-3">
-                <span className="text-accent text-3xl">Senior Full Stack Developer</span>
-                {/* <BsCodeSlash role="img" aria-label="developer icon" className="rounded-full p-[4px] w-[35px] h-[35px] border border-secondary text-secondary" /> */}
+                <span className="text-white/50 text-3xl">Senior Full Stack Developer</span>
+                <BsCodeSlash role="img" aria-label="developer icon" className="rounded-full p-[4px] w-[35px] h-[35px] border border-secondary text-secondary" />
               </div>
               <div className="xl:max-w-[500px] mt-3 leading-8 text-white/50 text-[1.1rem]">
                 <p>
@@ -34,12 +34,11 @@ const Home: React.FC = () => {
                 <p className="mt-4">
                   Experienced in cloud technologies, DevOps, AI integration, and have a strong eye for design, all while consistently delivering high-quality, reliable software efficiently.
                 </p>
-                <ResumeButtonDownload className="mt-10" />
               </div>
             </div>
           </div>
           {/* Image and particles */}
-          <div className="relative flex justify-center xl:justify-end w-full pt-24 xl:pt-0 mt-12 xl:mt-0 xl:pr-28">
+          <div className="relative flex justify-center xl:justify-end w-full pt-24 xl:pt-0 mt-12 xl:mt-0 xl:pr-16">
             <div className="relative">
               <Image
                 className="h-auto sm:h-[570px] w-full sm:w-auto"
@@ -52,16 +51,16 @@ const Home: React.FC = () => {
             <ParticlesContainer className="absolute bottom-10 top-0 xl:top-[-100px] right-0 xl:right-[-50px] left-0 z-[-9999]" />
           </div>
         </main>
-        <div className="flex mt-12 xl:mt-20 justify-center">
+        {/* <div className="h-[1px] flex-1 bg-white/15 my-24"></div> */}
+        <div className="flex items-center justify-between mt-12 xl:mt-14">
+          <ResumeButtonDownload />
           <HeroIcons iconColor="stroke-secondary" arrowColor="text-accent" />
         </div>
-        {/* <div className="h-[1px] flex-1 bg-white/15 my-24"></div> */}
-
-        {/* <div className="relative mb-12 mt-24 text-center text-3xl text-white/50 after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-secondary">
-          <span className="relative z-10 bg-primary py-2 px-4 rounded-lg text-white/50">
+        <div className="relative mb-12 mt-24 text-2xl text-secondary after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-white/15">
+          <span className="relative z-10 bg-primary ml-8 pl-4 pr-4 rounded-lg text-white/50">
             Skills
           </span>
-        </div> */}
+        </div>
         <div className="flex mb-7 mt-32 gap-12">
           <div className="w-full mb-7">
             <div className="flex justify-between py-1 text-base text-white/50">
